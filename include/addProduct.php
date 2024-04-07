212f4c4e0b0286e32e8c2225745a62f8f1ef8357
EmiiSem <just_dragon29@mail.ru>
2024-04-07 22:06:25 +0300

    Change code

--- include/addProduct.php
+++ include/addProduct.php
@@ -2,19 +2,18 @@
     session_start();
 
     include "connect.php";
-
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    $model = $_POST['model'];
+    $name = htmlspecialchars($_POST['name']);
+    $price = htmlspecialchars($_POST['price']);
+    $model = htmlspecialchars($_POST['model']);
     $category = $_POST['category'];
-    $qty = $_POST['qty'];
+    $qty = htmlspecialchars($_POST['qty']);
 
-    $path = "assest/img/Products/" . $_FILES['file']['name'];
-    move_uploaded_file($_FILES['file']['tmp_name'], $path);
+    $path = "assest/img/Products/" . time() . "_" . $_FILES['file']['name'];
+    move_uploaded_file($_FILES['file']['tmp_name'], "../" . $path);
 
     if($name) {
         mysqli_query($db, "INSERT INTO `products` (`product_id`, `name`, `price`, `model`, `category`, `count`, `path`) VALUES (NULL, '$name', '$price', '$model', '$category', '$qty', '$path')");
-        header("Location: ../Admin_page.php");
+        header("Location: ../Admin_page.php?message=Товар добавлен на страницу");
     }
 
 ?>
